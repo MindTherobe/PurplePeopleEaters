27c0c7c9813409ecf199cef3ee2922086b5470b3
John J. Thompson V <johnjthompson5@gmail.com>
2013-05-11 12:01:49 -0400

    Added Folder Structure
    Boo yeah!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PurplePeopleEater::Application.config.secret_token = '8e3eeae2503ed7b2f70b5768da41c5ecc9de531ac719b6200dee589fdca33ff16e8efc750d294d41a437bf01cb85a3dcae531f4147034c0371c88f709ffe6631'
